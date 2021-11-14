13845a81a9796691e8e23907455d7c47984c31d5
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2021-11-14 17:52:06 +0500

    sixth task done

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,16 @@
 new Vue({
   el: '#exercise',
   data: {
-    attachedHighlight: true,
+    effectClasses: {
+      highlight: true,
+      shrink: false
+    },
     borderedElement: 'borderedElement',
-    shadowedElement: 'shadowedElement',
     userClass: '',
     circledElement: '',
     isShadowedCircle: '',
-    userInputStyle: 'gray'
+    userInputStyle: 'gray',
+    progressPersentage: 0
   },
   computed: {
     hightlightShrink: function() {
@@ -18,11 +21,25 @@ new Vue({
     },
     shadowedCircle: function() {
       return (this.isShadowedCircle == 'true') ? true : false;
-    }
+    },
+    
   },
   methods: {
     startEffect: function() {
-      this.attachedHighlight = !this.attachedHighlight;
+      let vm = this;
+      this.progress = setInterval(function() {
+        vm.effectClasses.highlight = !vm.effectClasses.highlight;
+        vm.effectClasses.shrink = !vm.effectClasses.shrink;
+      },1000);
+    },
+    progressStarted: function() {
+      let vm = this;
+      // setInterval(function() {
+      //   return vm.progressPersentage += 10; 
+      // }, 1000);
+      setInterval(function(){
+        return vm.progressPersentage = (Math.random() * 101);
+      }, 1000);
     }
   }
 });
