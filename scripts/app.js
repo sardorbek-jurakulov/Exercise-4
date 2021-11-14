28613918a061b6e21269f7ef1f08e3eef9a1695e
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2021-11-14 09:29:34 +0500

    third task done

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,22 @@
 new Vue({
   el: '#exercise',
   data: {
-
+    attachedHighlight: true,
+    borderedElement: 'borderedElement',
+    shadowedElement: 'shadowedElement',
+    userClass: ''
+  },
+  computed: {
+    hightlightShrink: function() {
+      return {
+        highlight: this.attachedHighlight,
+        shrink: !this.attachedHighlight
+      }
+    }
   },
   methods: {
     startEffect: function() {
-    
+      this.attachedHighlight = !this.attachedHighlight;
     }
   }
 });
